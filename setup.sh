e870385c52c783da073741b9cc2e7b10c75ab605
mli42 <mli@student.42.fr>
2023-01-15 23:36:39 +0100

    fix(setup): More visible passwd prompt + remove deb

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ VBOX_DEB="virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb"
 # Set oneself as sudoers
 (su root -c "echo -e '$(whoami)\tALL=(ALL:ALL) ALL' >> /etc/sudoers")
 
+sudo echo 'Installing Vagrant and VBox...'
+
 # Install Vagrant
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
@@ -15,3 +17,4 @@ sudo apt update && sudo apt install vagrant=2.3.4
 # Install VBox
 wget https://download.virtualbox.org/virtualbox/7.0.4/${VBOX_DEB}
 sudo apt-get install -y ./${VBOX_DEB}
+rm ./${VBOX_DEB}

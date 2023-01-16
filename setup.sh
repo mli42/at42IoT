f69735f28595c1d8268a2d7f0c8e9fb860456cb2
mli42 <mli@student.42.fr>
2023-01-16 17:33:50 +0100

    feat(p2): Cluster with replicas and ingress

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 VBOX_DEB="virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb"
+SERVER_IP="192.168.56.110"
 
 # Set oneself as sudoers
 (su root -c "echo -e '$(whoami)\tALL=(ALL:ALL) ALL' >> /etc/sudoers")
@@ -18,3 +19,13 @@ sudo apt update && sudo apt install vagrant=2.3.4
 wget https://download.virtualbox.org/virtualbox/7.0.4/${VBOX_DEB}
 sudo apt-get install -y ./${VBOX_DEB}
 rm ./${VBOX_DEB}
+
+# Install curl and prerequisite to test p2
+sudo apt-get install -y curl
+
+sudo bash -c "cat >> /etc/hosts <<-EOF
+${SERVER_IP} app1.com
+${SERVER_IP} app2.com
+${SERVER_IP} otherapp.com
+EOF
+"
